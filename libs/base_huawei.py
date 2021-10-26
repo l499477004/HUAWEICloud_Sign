10bb3f78eb04d4b4c5ba3548989394eb54ddf017
l499477004 <499477004@qq.com>
2021-10-26 08:56:26 +0800

    Update base_huawei.py

--- libs/base_huawei.py
+++ libs/base_huawei.py
@@ -322,18 +322,18 @@ class BaseHuaWei(BaseClient):
         await self.task_page.click('.devui-layout-main-content #create_new_task')
         await asyncio.sleep(1)
         await self.task_page.click('.button-group .devui-btn-stress')
-        await self.logger.info("选择构建模板")
+        self.logger.info("选择构建模板")
         await asyncio.sleep(5)
         template = await self.task_page.querySelectorAll('.template-content li.template-item')
-        await self.logger.info("选择空白构建")
+        self.logger.info("选择空白构建")
         await template[3].click()
         await asyncio.sleep(1)
         await self.task_page.click('.button-group .devui-btn-stress')
         await asyncio.sleep(5)
-        await self.logger.info("添加步骤")
+        self.logger.info("添加步骤")
         await self.task_page.evaluate(
                 '''() =>{ document.querySelector('#app-devcloud-frameworks > div > ng-component > ng-component > div > edit > d-fullscreen > div > div > div.positon-relative.buildstep-wrapper.ng-star-inserted > d-splitter > d-splitter-pane:nth-child(2) > div > div > div > extend-plugins-render > div > extend-plugins-list > div.task-detail-cardlist.fn-clear-float > div:nth-child(3)').click() }''')
-        await self.logger.info("添加shell")
+        self.logger.info("添加shell")
         await asyncio.sleep(3)
         await self.task_page.click('.task-detail-cardlist .card-li:nth-child(3) .add-btn')
         await asyncio.sleep(2)
@@ -425,35 +425,104 @@ class BaseHuaWei(BaseClient):
         await asyncio.sleep(3)
 
     async def week_new_deploy_task(self):
-        await asyncio.sleep(5)
+        await asyncio.sleep(8)
         await self.task_page.click('#taskCreate')
+        self.logger.info("新建任务")
         await asyncio.sleep(3)
         await self.task_page.evaluate(
             '''() =>{ document.querySelector('#app-devcloud-frameworks > div > ng-component > div > create-new-form > div > div.deployman-create-warpper.position-relative > div.content-center > div > div > d-button.ng-star-inserted > button').click(); }''')
-        await asyncio.sleep(1)
+        self.logger.info("next")
+        await asyncio.sleep(2)
         await self.task_page.evaluate(
             '''() =>{ document.querySelector('#app-devcloud-frameworks > div > ng-component > div > create-new-form > div > div.deployman-create-warpper.position-relative > div.content-center > div > choose-template > div.content > div > div > div:nth-child(2) > div.template-list > div:nth-child(1) > div').click(); }''')
+        self.logger.info("选择空白模板")
         await asyncio.sleep(1)
         await self.task_page.evaluate(
             '''() =>{ document.querySelector('#app-devcloud-frameworks > div > ng-component > div > create-new-form > div > div.deployman-create-warpper.position-relative > div.content-center > div > div > d-button:nth-child(2) > button').click(); }''')
-        await asyncio.sleep(1)
+        self.logger.info("next")
+        await asyncio.sleep(3)
         await self.task_page.type('#app-devcloud-frameworks > div > ng-component > div > template-form > div > form > deploy-step > div > div.operation-box__plugins-list.ng-star-inserted > extend-plugins-render > div > div.task-detail-title.margin-bottom-20.fn-clear-float.plugin-titile > d-search > div > input', "shell")
+        self.logger.info("输入shell")
         await asyncio.sleep(1)
         await self.task_page.evaluate(
             '''() =>{ document.querySelector('#app-devcloud-frameworks > div > ng-component > div > template-form > div > form > deploy-step > div > div.operation-box__plugins-list.ng-star-inserted > extend-plugins-render > div > div.task-detail-title.margin-bottom-20.fn-clear-float.plugin-titile > d-search > div > span.devui-search-icon').click(); }''')
+        self.logger.info("搜索")
         await asyncio.sleep(3)
         await self.task_page.evaluate(
             '''() =>{ document.querySelector('#app-devcloud-frameworks > div > ng-component > div > template-form > div > form > deploy-step > div > div.operation-box__plugins-list.ng-star-inserted > extend-plugins-render > div > extend-plugins-list > div.task-detail-cardlist.fn-clear-float > div:nth-child(1) > div.btn-wrapper > span').click(); }''')
+        self.logger.info("选择shell")
+        await asyncio.sleep(1)
+
+
+
+        NewUrl = await self.task_page.evaluate(
+            '''() =>{ document.querySelector('#DeploymentGroup_groupId > label > div > div > span > span:nth-child(3) > span > a').attributes["href"].nodeValue; }''')
+        self.logger.info("新建主机组" + NewUrl)
+        await newHostGroup(self, NewUrl)
+        await self.task_page.click("#DeploymentGroup_groupId_button")
+        self.logger.info("刷新主机组")
+
+        await self.task_page.evaluate(
+            '''() =>{ document.querySelector('#DeploymentGroup_groupId > div > div.devui-form-group.devui-has-feedback > input').click(); }''')
+        self.logger.info("选择主机组")
         await asyncio.sleep(1)
         await self.task_page.evaluate(
-            '''() =>{ document.querySelector('#DeploymentGroup_groupId > div > div.devui-form-group.devui-has-feedback.ng-tns-c78-38 > input').click(); }''')
-        await asyncio.sleep(1)
-        await self.task_page.evaluate(
-            '''() =>{ document.querySelector('#DeploymentGroup_groupId > div > div.devui-dropdown-menu.ng-trigger.ng-trigger-fadeInOut.ng-tns-c78-38.ng-star-inserted > ul > ul > li').click(); }''')
+            '''() =>{ document.querySelector('#DeploymentGroup_groupId > div > div.devui-dropdown-menu.ng-trigger.ng-trigger-fadeInOut.ng-star-inserted > ul > ul > li').click(); }''')
         await asyncio.sleep(1)
         await self.task_page.evaluate(
             '''() =>{ document.querySelector('#app-devcloud-frameworks > div > ng-component > div > template-form > div > form > div.deployman-create-content__button-group.ng-star-inserted > d-button:nth-child(2) > button').click(); }''')
         await asyncio.sleep(1)
+
+    async def newHostGroup(self, newHostGroupUrl):
+        page = await self.browser.newPage()
+        await page.goto(newHostGroupUrl, {'waitUntil': 'load'})
+        self.logger.info("--主机组管理")
+        await asyncio.sleep(5)
+        try：
+            await page.type('#app-devcloud-frameworks > div > ng-component > div > hostgroup-info > div > d-tabs > div > div > basic-info > div > div > div:nth-child(1) > input', "linux")
+            self.logger.info("--设置主机名称")
+            await asyncio.sleep(1)
+            await page.evaluate(
+                '''() =>{ document.querySelector('#app-devcloud-frameworks > div > ng-component > div > hostgroup-info > div > d-tabs > div > div > basic-info > div > div > div.btn-box > div.margin-right-s > d-button > button').click(); }''')
+            self.logger.info("--保存信息")
+            await asyncio.sleep(3)
+            await page.click("#addHostBtn")
+            self.logger.info("--添加主机")
+            await asyncio.sleep(3)
+            await page.type('#addHostDialog > div > div > d-modal-container > div > div > div > ng-component > div > div:nth-child(3) > div.ext-form-item-content > input', "linux-LYT")
+            self.logger.info("--设置主机名称")
+            await asyncio.sleep(1)
+            await page.type('#addHostDialog > div > div > d-modal-container > div > div > div > ng-component > div > div:nth-child(5) > div.ext-form-item-content > input', "47.92.216.67")
+            self.logger.info("--设置主机IP")
+            await asyncio.sleep(1)
+            await page.type('#addHostDialog > div > div > d-modal-container > div > div > div > ng-component > div > div:nth-child(13) > div.ext-form-item-content > input', "user"+self.username)
+            self.logger.info("--设置主机用户名")
+            await asyncio.sleep(1)
+            await page.type('#addHostDialog > div > div > d-modal-container > div > div > div > ng-component > div > div:nth-child(14) > div.ext-form-item-content > d-form-control > div.devui-form-control-container > input', self.password)
+            self.logger.info("--设置主机密码")
+            await asyncio.sleep(1)
+            await page.type('#addHostDialog > div > div > d-modal-container > div > div > div > ng-component > div > div:nth-child(15) > div.ext-form-item-content > input', "22")
+            self.logger.info("--设置主机端口")
+            await asyncio.sleep(1)
+            await page.evaluate(
+                '''() =>{ document.querySelector('#addHostDialog > div > div > d-modal-container > div > div > div > ng-component > div > label > d-checkbox > div > label > span').click(); }''')
+            self.logger.info("--勾选隐私条款")
+            await asyncio.sleep(1)
+            await page.evaluate(
+                '''() =>{ document.querySelector('#addHostDialog > div > div > d-modal-container > div > div > div > ng-component > div > div.button-container > d-button.mr10.ng-star-inserted > button').click(); }''')
+            self.logger.info("--确认添加主机")
+            self.logger.info("--等待连通性验证")
+            await asyncio.sleep(10)
+        except Exception as e:
+            self.logger.error(e)
+            await page.close()
+        finally:
+            await page.close()
+
+
+        
+
+
 
     async def deploy_task(self):
         await asyncio.sleep(3)
